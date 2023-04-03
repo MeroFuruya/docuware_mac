cb7aedbe4742d669126bbaf28a088b090ab81637
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2023-04-03 09:41:12 +0200

    move task record to its own file

---
+++ task.pas
@@ -0,0 +1,28 @@
+unit task;
+
+interface
+
+type
+  TTask = record
+    guid: string;
+    command: string;
+    archive: string;
+    archiveId: string;
+    selection: string;
+    error: string;
+    class function Create(guid, command, archive, selection: string): TTask; static;
+  end;
+
+implementation
+
+{ TTask }
+
+class function TTask.Create(guid, command, archive, selection: string): TTask;
+begin
+  Result.guid := guid.ToLower;
+  Result.command := command.ToLower;
+  Result.archive := ExtractFileName(archive).ToLower;
+  Result.selection := selection.ToLower;
+end;
+
+end.
